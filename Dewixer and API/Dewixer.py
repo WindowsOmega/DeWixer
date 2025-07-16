bb01612dd01df0c1a1088422fcdb27d584a18261
WindowsOmega <166148619+WindowsOmega@users.noreply.github.com>
2025-07-16 14:59:36 -0400

    Added Custom JS and fixed archives

--- Dewixer and API/Dewixer.py
+++ Dewixer and API/Dewixer.py
@@ -77,6 +77,7 @@ def curl_get_and_save(url, filename, wts):
             while True:
                 if os.path.exists(dated_folder + f"\\{n_filename}"):
                     n_filename = n_filename.replace(".html", "")
+                    n_filename = n_filename.replace(f" ({rounds-1})", "")
                     n_filename = n_filename + f" ({rounds})" + ".html"
                     rounds = rounds + 1
                 else:
@@ -174,10 +175,7 @@ def dewixer():
                 time.sleep(0.01)
 prog_bar.update(12.5)          
 
-word_to_delete = '<div id="WIX_ADS" class="EFLBov czJOIz ytGGBw"><a data-testid="linkElement" href="//www.wix.com/lpviral/enviral?utm_campaign=vir_wixad_live&amp;orig_msid=ab72da44-75fc-4752-9bcf-113720cbc616&amp;adsVersion=white" target="_blank" rel="nofollow" class="Oxzvyr YD5pSO has-custom-focus"><span class="aGHwBE"><span data-hook="freemium-text" class="areOb6">This site was designed with the <div data-testid="bannerLogo" class="dTTUA9"><div><svg class="_4i7Zy" viewBox="0 0 28 10.89" aria-label="wix"><path d="M16.02.2c-.55.3-.76.78-.76 2.14a2.17 2.17 0 0 1 .7-.42 3 3 0 0 0 .7-.4A1.62 1.62 0 0 0 17.22 0a3 3 0 0 0-1.18.2z" class="o4sLYL"></path><path d="M12.77.52a2.12 2.12 0 0 0-.58 1l-1.5 5.8-1.3-4.75a4.06 4.06 0 0 0-.7-1.55 2.08 2.08 0 0 0-2.9 0 4.06 4.06 0 0 0-.7 1.55L3.9 7.32l-1.5-5.8a2.12 2.12 0 0 0-.6-1A2.6 2.6 0 0 0 0 .02l2.9 10.83a3.53 3.53 0 0 0 1.42-.17c.62-.33.92-.57 1.3-2 .33-1.33 1.26-5.2 1.35-5.47a.5.5 0 0 1 .34-.4.5.5 0 0 1 .4.5c.1.3 1 4.2 1.4 5.5.4 1.5.7 1.7 1.3 2a3.53 3.53 0 0 0 1.4.2l2.8-11a2.6 2.6 0 0 0-1.82.53zm4.43 1.26a1.76 1.76 0 0 1-.58.5c-.26.16-.52.26-.8.4a.82.82 0 0 0-.57.82v7.36a2.47 2.47 0 0 0 1.2-.15c.6-.3.75-.6.75-2V1.8zm7.16 3.68L28 .06a3.22 3.22 0 0 0-2.3.42 8.67 8.67 0 0 0-1 1.24l-1.34 1.93a.3.3 0 0 1-.57 0l-1.4-1.93a8.67 8.67 0 0 0-1-1.24 3.22 3.22 0 0 0-2.3-.43l3.6 5.4-3.7 5.4a3.54 3.54 0 0 0 2.32-.48 7.22 7.22 0 0 0 1-1.16l1.33-1.9a.3.3 0 0 1 .57 0l1.37 2a8.2 8.2 0 0 0 1 1.2 3.47 3.47 0 0 0 2.33.5z"></path></svg></div><div class="uJDaUS">.com</div></div> website builder. Create your website today.</span><span data-hook="freemium-button" class="O0tKs2 Oxzvyr">Start Now</span></span></a></div>'
 find_and_delete_word()
-if debug_flag == 1:
-    print(f"'{word_to_delete}' deleted from '{filepath}'")
 dewixer()
 
 time.sleep(1)
@@ -215,16 +213,7 @@ matches = find_dynamic_key_value_pairs(filepath)
 print("Found embeds:", matches)
 matchess = 0
 times_embed = len(matches)
-def extract_div_content(file_path):
 
-    div_pattern = r'<div id=".*?" class=".*?">(.*?)</div>'
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-
-    matches2 = re.findall(div_pattern, content, re.DOTALL)
-
-    return matches2
 
 def find_and_replace_div(input_file_path, output_file_path, div_id, new_content):
     with open(input_file_path, 'r', encoding='utf-8') as file:
@@ -269,6 +258,45 @@ for i in range(times_embed):
             find_and_replace_div(filepath, filepath, embed_cur, cur_wixiframe)
             matchess = matchess + 1
             pass
+
+def read_file_and_process(file_name):
+    with open(file_name, 'r') as file:
+        number_of_files = int(file.readline().strip())
+        file_paths = []
+        for _ in range(number_of_files):
+            file_path = file.readline().strip()
+            file_paths.append(file_path)
+        return file_paths
+
+def js_div(input_file_path, output_file_path, div_id, new_content):
+    with open(input_file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    pattern = rf'<div class="[^"]*" id="{div_id}">.+?</div>'
+    print(pattern)
+    matches3 = re.findall(pattern, content, re.DOTALL)
+
+    if matches3:
+        updated_content = re.sub(pattern, new_content, content, flags=re.DOTALL)
+
+        with open(output_file_path, 'w', encoding='utf-8') as file:
+            file.write(updated_content)
+        print("Content replaced successfully.")
+
+def remove_path(file_paths, path_to_remove):
+    if path_to_remove in file_paths:
+        file_paths.remove(path_to_remove)
+        print(f"Removed: {path_to_remove}")
+    else:
+        print(f"Path '{path_to_remove}' not found in the list.")
+
+js_filename = read_file_and_process(os.path.expanduser("~\\Documents") + "\\customjs.txt")
+for path in js_filename:
+    js_div_name = path.replace(".txt", "")
+    with open(os.path.expanduser("~\\Documents") + f"\\{path}", 'r', encoding='utf-8') as file:
+        content = file.read()
+    js_div(filepath, filepath, js_div_name, content)
+
 
 if '.txt' in filepath:
     oldfilepath = filepath
